7dc6a3319bc83d2ee7b382ead742551268bb29f3
Yeseo <superior817@naver.com>
2020-06-23 18:50:20 +0900

    프로젝트 내용 추가

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,9 @@ import styles from "./Project.module.css";
 import { Row, Col } from "reactstrap";
 import CoinoneCoreImg from "../images/p1.png";
 import MembershipImg from "../images/p2.png";
+import XgreenImg from "../images/p3.png";
+import GurufitGodoImg from "../images/p4.png";
+import GurufitBetaImg from "../images/p5.png";
 
 class Project extends Component {
   render() {
@@ -23,6 +26,9 @@ class Project extends Component {
                 <dd>React로 만든 코인원 코어 데모 사이트입니다.</dd>
               </dl>
             </div>
+            <a href="#" className={styles.btnBlue}>
+              구현 기능 상세보기
+            </a>
           </Col>
         </Row>
         <Row className={styles.projectContent}>
@@ -39,6 +45,70 @@ class Project extends Component {
                 <dd>React로 구성되어진 사이트입니다.</dd>
               </dl>
             </div>
+            <a href="#" className={styles.btnBlue}>
+              구현 기능 상세보기
+            </a>
+          </Col>
+        </Row>
+        <Row className={styles.projectContent}>
+          <Col md="3"></Col>
+          <Col md="9">
+            <img
+              src={XgreenImg}
+              alt="xgreen 사이트"
+              className={styles.projectImgSize}
+            />
+            <div className="mt-5">
+              <dl>
+                <dt>Xgreen 거래소</dt>
+                <dd>
+                  React로 구성되어진 사이트입니다.
+                  <br />
+                  지금 현재는 랜딩 페이지만 남아있는 상태입니다.
+                </dd>
+              </dl>
+            </div>
+            <a href="#" className={styles.btnBlue}>
+              구현 기능 상세보기
+            </a>
+          </Col>
+        </Row>
+        <Row className={styles.projectContent}>
+          <Col md="3"></Col>
+          <Col md="9">
+            <img
+              src={GurufitGodoImg}
+              alt="구루핏 고도몰"
+              className={styles.projectImgSize}
+            />
+            <div className="mt-5">
+              <dl>
+                <dt>구루핏 고도몰 사이트</dt>
+                <dd>고도몰로 구축되어진 사이트입니다.</dd>
+              </dl>
+            </div>
+            <a href="#" className={styles.btnBlue}>
+              구현 기능 상세보기
+            </a>
+          </Col>
+        </Row>
+        <Row className={styles.projectContent}>
+          <Col md="3"></Col>
+          <Col md="9">
+            <img
+              src={GurufitBetaImg}
+              alt="구루핏 베타"
+              className={styles.projectImgSize}
+            />
+            <div className="mt-5">
+              <dl>
+                <dt>구루핏 베타</dt>
+                <dd>HTML, CSS, jQuery로 구축되어진 사이트입니다.</dd>
+              </dl>
+            </div>
+            <a href="#" className={styles.btnBlue}>
+              구현 기능 상세보기
+            </a>
           </Col>
         </Row>
       </React.Fragment>
